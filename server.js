fffabea1bcd9ed8faad1fa286698721a08457f67
Enrico Alvarenga <enricomalvarenga@gmail.com>
2016-07-05 19:20:14 -0300

    PUT http

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.get('/todos/:id', function(req, res) {
 
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'desc', 'completed');
-	console.log(body);
 	if (!_.isBoolean(body.completed) || !_.isString(body.desc) || body.desc.trim().length === 0) {
 		return res.status(400).send();
 	}
@@ -45,8 +44,35 @@ app.delete('/todos/:id', function(req, res) {
 	 if(todos) {
 		 res.json(todos);
 	 } else {
-		 res.status(404).json('No todo found to be deleted');
+		 res.status(404).json("No todo found to be deleted");
 	 }
+})
+
+app.put('/todos/:id', function(req, res) {
+	var id = parseInt(req.params.id, 10);
+	var updatedTodo = _.findWhere(todos, {id: id});
+	var body = _.pick(req.body, 'desc', 'completed');
+	var validAtt = {};
+
+	if(!updatedTodo) {
+		return res.status(404).json("Something went wrong!");
+	}
+			if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+				validAtt.completed = body.completed;
+			} else if(body.hasOwnProperty('completed')) {
+						return res.status(400).json("Error");
+			}
+			if(body.hasOwnProperty('desc') && _.isString(body.desc) && body.desc.trim().length > 0) {
+				validAtt.desc = body.desc;
+			} else if (body.hasOwnProperty('desc')) {
+						return res.status(400).json("Error");
+			}
+
+
+			_.extend(updatedTodo, validAtt);
+			res.json(updatedTodo);
+
+
 })
 
 app.listen(PORT, function() {
