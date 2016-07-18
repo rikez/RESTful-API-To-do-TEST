1bc8053cf531cd845474250e85b6f13fab7ad4e7
Enrico Alvarenga <enricomalvarenga@gmail.com>
2016-07-18 14:21:06 -0300

    Hashing & Salting

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
 module.exports = function(sequelize, dataTypes) {
   return sequelize.define('user', {
     name: {
@@ -15,13 +18,41 @@ module.exports = function(sequelize, dataTypes) {
         isEmail: true
       }
     },
+    salt: {
+      type: dataTypes.STRING
+    },
+    pass_hash: {
+      type: dataTypes.STRING
+    },
     pass: {
-      type: dataTypes.STRING,
+      type: dataTypes.VIRTUAL,
       allowNull: false,
       validate: {
         len: [6, 50]
+      },
+      set: function(value) {
+        var salt = bcrypt.genSaltSync(10);
+        var hashedPass = bcrypt.hashSync(value, salt);
+
+        this.setDataValue('pass', value);
+        this.setDataValue('salt', salt);
+        this.setDataValue('pass_hash', hashedPass);
       }
     }
 
+  }, {
+    hooks: {
+        beforeValidate: function(user, options) {
+          if(typeof user.email === 'string') {
+            user.email = user.email.toLowerCase();
+          }
+        }
+    },
+    instanceMethods: {
+      toPublicJSON: function () {
+        var json = this.toJSON();
+        return _.pick(json, 'id', 'name', 'email', 'createdAt', 'updatedAt');
+      }
+    }
   });
 }
--- server.js
+++ server.js
@@ -103,13 +103,13 @@ app.put('/todos/:id', function(req, res) {
 app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'name', 'email', 'pass');
 	db.user.create(body).then(function(user) {
-			res.json(user.toJSON());
+			res.json(user.toPublicJSON());
 	}, function(e) {
 			res.status(404).json(e);
 	})
 })
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Listening on port ' + PORT);
 	});
